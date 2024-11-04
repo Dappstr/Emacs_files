a1891e89c3b331d5812964d725733316d49eab6b
Dappster <87837549+Dappstr@users.noreply.github.com>
2024-11-04 15:04:59 -0800

    Update init.el

--- windows/init.el
+++ windows/init.el
@@ -63,13 +63,21 @@
  '(custom-safe-themes
    '("3e6dc9dfb08b94357a348332644592e59e5292cc877910072ab985680c91edec" default))
  '(package-selected-packages
-   '(rainbow-delimiters multiple-cursors zig-mode ligature org tree-sitter-langs)))
+   '(company eglot rainbow-delimiters multiple-cursors zig-mode ligature org tree-sitter-langs)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "red"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "orange"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "green"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "light sky blue"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "violet"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "purple"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "cyan"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "magenta")))))
 
 (require 'org) ;; Org mode
 
@@ -110,16 +118,31 @@
 
 ;; Load and configure rainbow-delimiters
 (require 'rainbow-delimiters)
-(custom-set-faces
- '(rainbow-delimiters-depth-1-face ((t (:foreground "red"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "orange"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "yellow"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "green"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "light sky blue"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "violet"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "purple"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "cyan"))))
- '(rainbow-delimiters-depth-9-face ((t (:foreground "magenta")))))
+
 
 ;; Enable rainbow-delimiters in all programming modes
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+;; For getting the keys to install eglot
+(setq epg-gpg-program "C:/Program Files (x86)/GnuPG/bin/gpg.exe")
+
+;; Eglot
+(require 'eglot)
+(add-to-list 'eglot-server-programs '((c-mode c++-mode) . ("clangd")))
+(add-to-list 'eglot-server-programs '(zig-mode . ("zls")))
+
+;; Enable Company Mode globally
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Configure Company Mode to work well with Eglot
+(with-eval-after-load 'company
+  (setq company-minimum-prefix-length 1
+        company-idle-delay 0.0)) ;; Instant completion (adjust if needed)
+
+;; Use Company Mode with Eglot
+(add-hook 'eglot-managed-mode-hook (lambda () (company-mode)))
+
+;; Automatically start Eglot for C, C++, and Zig files
+(add-hook 'c-mode-hook 'eglot-ensure)
+(add-hook 'c++-mode-hook 'eglot-ensure)
+(add-hook 'zig-mode-hook 'eglot-ensure)
